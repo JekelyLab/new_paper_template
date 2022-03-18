cd9f7bab0af909af1e88cb0600fec93027005e98
Gaspar Jekely <g.jekely@exeter.ac.uk>
2022-03-18 21:56:38 +0000

    Fig1: updated with patchwork panel assembly

--- code/Figure1.R
+++ code/Figure1.R
@@ -74,11 +74,15 @@ readr::write_csv(syn_tb, file="supplements/Supplementary_table1.csv", na="", quo
 
 # assemble figure ---------------------------------------------------------
 
-#read png convert to image panel
-panelA <- ggdraw() + draw_image(readPNG("pictures/Platynereis_SEM_inverted_nolabel.png"), scale = 1) + 
-  draw_label("Platynereis larva", x = 0.5, y = 0.95, fontfamily = "sans", fontface = "plain",
+#read the pictures from the /pictures folder
+img1 <- readPNG("pictures/Platynereis_SEM_inverted_nolabel.png")
+img2 <- readPNG("pictures/head_celltypes_syn_matrix.png")
+
+#convert to image panel and add text labels with cowplot::draw_image 
+panelA <- cowplot::ggdraw() + cowplot::draw_image(img1, scale = 1) + 
+  draw_label("Platynereis larva", x = 0.35, y = 0.99, fontfamily = "sans", fontface = "plain",
              color = "black", size = 11, angle = 0, lineheight = 0.9, alpha = 1) +
-  draw_label(expression(paste("50 ", mu, "m")), x = 0.2, y = 0.11, fontfamily = "sans", fontface = "plain",
+  draw_label(expression(paste("50 ", mu, "m")), x = 0.27, y = 0.05, fontfamily = "sans", fontface = "plain",
              color = "black", size = 10, angle = 0, lineheight = 0.9, alpha = 1) + 
   draw_label("head", x = 0.5, y = 0.85, fontfamily = "sans", fontface = "plain",
              color = "black", size = 9, angle = 0, lineheight = 0.9, alpha = 1) + 
@@ -92,25 +96,68 @@ panelA <- ggdraw() + draw_image(readPNG("pictures/Platynereis_SEM_inverted_nolab
              color = "black", size = 9, angle = 0, lineheight = 0.9, alpha = 1) + 
   draw_label("pygidium", x = 0.55, y = 0.03, fontfamily = "sans", fontface = "plain",
              color = "black", size = 9, angle = 0, lineheight = 0.9, alpha = 1)
-panelB <- ggdraw() + draw_image(readPNG("pictures/head_celltypes_syn_matrix.png"), scale = 1) + 
-  draw_label("Synaptic connectivity of head cell types", x = 0.45, y = 0.97, fontfamily = "sans", fontface = "plain",
-             color = "black", size = 8, angle = 0, lineheight = 0.9, alpha = 1)
+panelB <- ggdraw() + draw_image(img2, scale = 1) + 
+  draw_label("Synaptic connectivity", x = 0.4, y = 0.99, fontfamily = "sans", fontface = "plain",
+             color = "black", size = 11, angle = 0, lineheight = 0.9, alpha = 1)
+
+#clear the images from memory
+rm(img1,img2)
 
 
-#combine panels into Figure and save final figure as pdf and png
-Fig1 <- plot_grid(panelA,panelB,
-                  ncol=2,
-                  rel_widths = c(0.4, 1),
-                  labels=c("A", "B"),
-                  label_size = 12, label_y = 1, label_x = 0,
-                  label_fontfamily = "sans", label_fontface = "plain") + 
-  theme(plot.margin = unit(c(1, 1, 1, 1), units = "pt"))
+# assemble final  figure with patchwork -----------------------------------
 
-#save
+#we use the very efficient and elegant patchwork package to assemble the 
+#multi-panel figure
+
+#first, we define the layout with textual representation
+# the # symbol will introduce an empty field - can be used to add spacers,
+#their width will be defined later with the widths/heights options
+
+layout <- "
+ABCD
+####
+EFKK
+##KK
+IJKK
+"
+
+#same as
+layout2 <- "
+AABBCCDD
+AABBCCDD
+########
+########
+EEFFKKKK
+EEFFKKKK
+####KKKK
+####KKKK
+IIJJKKKK
+IIJJKKKK
+"
+#you get the picture - you can define any layout and ratios with this representation
+
+#we now define which panel go into the figure and in which order, the layout
+#will be based on our textual definition above
+#we can also define the relative sizes of rows and columns and the tags to use
+#it is not necessary to enter all the tags e.g., c("A", "B", "C", "D"), patchwork
+#takes care of that if we define the tag_levels only
+Figure1 <- panelA + panelB + panelA + panelB + 
+  panelA + panelB + panelB +  
+  panelA + panelB +  
+  patchwork::plot_layout(design = layout2, heights = c(1, 1, 0.05, 0.05, 1, 1, 0.05, 0.05, 1, 1)) + #we can change the heights of the rows in our layout (widths also can be defined)
+  patchwork::plot_annotation(tag_levels = "A") &  #we can change this to 'a' for small caps or 'i' or '1'
+  ggplot2::theme(plot.tag = element_text(size = 12, face='plain')) #or 'bold', 'italic'
+
+
+# save figure -------------------------------------------------------------
+
+#when you save the figure as pdf and png it is important to set the width and
+#height of the figure properly, so that your panels fit nicely - you may have to try a few times
+
 ggsave("figures/Figure1.pdf", limitsize = FALSE, 
-       units = c("px"), Fig1, width = 1800, height = 850)
+       units = c("px"), Figure1, width = 3300, height = 2600)
 ggsave("figures/Figure1.png", limitsize = FALSE, 
-       units = c("px"), Fig1, width = 1800, height = 850, bg='white')
+       units = c("px"), Figure1, width = 3000, height = 1700, bg = "white")
 
 
 
