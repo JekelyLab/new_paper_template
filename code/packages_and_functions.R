385d35f7486d7140dfa50b7b110fc76013d8a946
Gaspar Jekely <g.jekely@exeter.ac.uk>
2022-03-23 12:52:33 +0000

    packages_and_functions.R to source added

---
+++ code/packages_and_functions.R
@@ -0,0 +1,8 @@
+rm(list = ls(all.names = TRUE)) #will clear all objects includes hidden objects.
+gc() #free up memory and report the memory usage.
+
+# load some packages
+library(tidyverse)
+library(cowplot)
+library(png)
+library(patchwork)
