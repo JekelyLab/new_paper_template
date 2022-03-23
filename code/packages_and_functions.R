5be27cfb5ecf924e5bcc89c83860315011a1abd0
Gaspar Jekely <g.jekely@exeter.ac.uk>
2022-03-23 13:57:47 +0000

    packages and functions: added color palettes

--- code/packages_and_functions.R
+++ code/packages_and_functions.R
@@ -7,6 +7,15 @@ library(cowplot)
 library(png)
 library(patchwork)
 
+#define some colors
+#From Color Universal Design (CUD): https://jfly.uni-koeln.de/color/
+Okabe_Ito <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", 
+               "#CC79A7", "#000000")
+########################## SHOW as multiple PIE Charts ###################
+pie(rep(1,8), col=Okabe_Ito, Okabe_Ito, main='Okabe Ito')
+blues <- brewer.pal(9, 'Blues')
+bluepurple <- brewer.pal(9, 'BuPu')
+oranges <- brewer.pal(9, 'YlOrRd')
 
 #save session info and Rstudio version info for reproducibility
 writeLines(capture.output(sessionInfo()), "code/sessionInfo.txt")
